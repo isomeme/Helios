7fe7ff7c9f95826a68ec19c4035b66a4b920dfaa
Catherine Berry <35911229+isomeme@users.noreply.github.com>
2020-06-07 21:30:14 -0700

    Yay! Full sun-events display is in place. Removed TextView showing debug info.
    Boo! SunCal 3.1 still produces noon errors of nearly a degree. Added test to demonstrate.

--- helios/src/test/java/org/onereed/helios/sun/SunCalcTest.java
+++ helios/src/test/java/org/onereed/helios/sun/SunCalcTest.java
@@ -7,14 +7,14 @@ import org.shredzone.commons.suncalc.SunTimes;
 import java.time.Instant;
 import java.time.ZonedDateTime;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 /** Tests demonstrating bugs in SunCalc */
 public class SunCalcTest {
 
   @Test
-  public void noonFarFrom180() {
+  public void noonNear180_bad() {
     double[] where = {34.0, -118.5};
     Instant when = Instant.parse("2020-06-08T04:17:00Z");
     SunTimes sunTimes = SunTimes.compute().at(where).on(when).execute();
@@ -24,6 +24,6 @@ public class SunCalcTest {
     SunPosition sunPosition = SunPosition.compute().at(where).on(noon).execute();
     double noonAzimuth = sunPosition.getAzimuth();
 
-    assertEquals(180.0, noonAzimuth, 0.25); // Fails, noonAzimuth = 180.96613610900388
+    assertNotEquals(180.0, noonAzimuth, 0.25); // noonAzimuth = 180.96613610900388
   }
 }
