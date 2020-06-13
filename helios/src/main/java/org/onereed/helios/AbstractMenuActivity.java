d7d2b1aa85f9060c04856c69107a5dbefbc8fec1
Catherine Berry <35911229+isomeme@users.noreply.github.com>
2020-06-12 22:10:53 -0700

    Bump version in preparation for alpha 05 release. Provide a 'Help' menu item on all activities that links to a new Helios support page.

--- helios/src/main/java/org/onereed/helios/AbstractMenuActivity.java
+++ helios/src/main/java/org/onereed/helios/AbstractMenuActivity.java
@@ -1,5 +1,7 @@
 package org.onereed.helios;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -33,13 +35,23 @@ abstract class AbstractMenuActivity extends AppCompatActivity {
       return true;
     }
 
+    if (item.getItemId() == R.id.action_help) {
+      Intent browserIntent =
+          new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.one-reed.org/helios"));
+      startActivity(browserIntent);
+      return true;
+    }
+
     return super.onOptionsItemSelected(item);
   }
 
   /**
    * Returns a {@link Map} from menu item resource IDs to the action which should be taken when that
    * menu item is selected. Only menu items with IDs that appear as keys in the map are made
-   * visible. This default implementation returns an empty map, meaning no menu items are shown.
+   * visible. This default implementation returns an empty map.
+   *
+   * <p>In addition to whatever is specified in this call, all menus have the 'Help' menu item,
+   * which links to the Helios webpage.
    */
   @ForOverride
   protected Map<Integer, Runnable> getMenuActions() {
