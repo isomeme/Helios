c2c0730d98c571cb1d3fa02453250571238017a2
Catherine Berry <cdberry@gmail.com>
2025-10-25 16:30:31 -0700

    feat: Improve `LifecycleLogger` with instance tracking.
    - Enhanced `LifecycleLogger` to differentiate between multiple instances of the same `LifecycleOwner` class.
    - Replaced the simple class name tag with a unique tag for each object instance, including an instance counter (e.g., `LC_MainActivity_1`, `LC_MainActivity_2`).
    - Used `HashMultiset` to track instance counts for each class name.
    - Refined the log output format for better clarity.

--- helios/src/main/java/org/onereed/helios/util/LifecycleLogger.kt
+++ helios/src/main/java/org/onereed/helios/util/LifecycleLogger.kt
@@ -1,20 +1,44 @@
 package org.onereed.helios.util
 
-import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.Lifecycle.Event
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
+import com.google.common.collect.HashMultiset
 import timber.log.Timber
+import java.util.Locale
 
 class LifecycleLogger  : LifecycleEventObserver {
 
-  override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+  private data class SourceId(val className: String, val hashCode: Int) {
+    constructor(source: LifecycleOwner) : this(source.javaClass.simpleName, source.hashCode())
+  }
+
+  override fun onStateChanged(source: LifecycleOwner, event: Event) {
+    val tag = getTag(source)
+    val state = event.targetState
+
     val dir = when (event) {
-      Lifecycle.Event.downTo(event.targetState) -> "\u2198" // ↘
-      Lifecycle.Event.upTo(event.targetState) -> "\u2197" // ↗
-      else -> "\u219d" // ↝
+      Event.downTo(state) -> '\u2198' // ↘
+      Event.upTo(state) -> '\u2197' // ↗
+      else -> '\u219d' // ↝
     }
 
-    Timber.tag("LCYC_${source.javaClass.simpleName}")
-      .d("%-10s %s %s", event, dir, event.targetState)
+    Timber.tag(tag).d("%-10s %c %s", event, dir, state)
+  }
+
+  companion object {
+
+    private val classInstanceCounts = HashMultiset.create<String>()
+    private val sourceIdTags = mutableMapOf<SourceId, String>()
+
+    fun getTag(source: LifecycleOwner): String {
+      val sourceId = SourceId(source)
+
+      return sourceIdTags.getOrPut(sourceId) {
+        // add() returns count before insertion, and we want 1-origin.
+        val instanceNumber = classInstanceCounts.add(sourceId.className, 1) + 1
+        String.format(Locale.US, "LC_%.18s_%d", sourceId.className, instanceNumber)
+      }
+    }
   }
 }
